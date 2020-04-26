da1db01ddfbda2e263f73f64282132f28b7af67c
Carter <carter@rocketmiles.com>
2020-04-26 15:43:07 -0700

    Add support for two approvers if two github tokens are provided

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,34 @@ import * as github from "@actions/github";
 
 async function run() {
   try {
-    const token = core.getInput("github-token", { required: true });
+    const tokenOne = core.getInput("github-token-one", { required: true });
+    const tokenTwo = core.getInput("github-token-two", { required: false });
 
     const { pull_request: pr } = github.context.payload;
     if (!pr) {
       throw new Error("Event payload missing `pull_request`");
     }
 
-    const client = new github.GitHub(token);
+    let client = new github.GitHub(tokenOne);
     core.debug(`Creating approving review for pull request #${pr.number}`);
     await client.pulls.createReview({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       pull_number: pr.number,
-      event: "APPROVE"
+      event: "APPROVE",
     });
     core.debug(`Approved pull request #${pr.number}`);
+
+    if (tokenTwo) {
+      client = new github.GitHub(tokenTwo);
+      await client.pulls.createReview({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        pull_number: pr.number,
+        event: "APPROVE",
+      });
+      core.debug(`Approved pull request #${pr.number} again`);
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
